10af8817a734d65f5b044f0130cfa2291103b841
DioneJM <dionemorales@outlook.com>
2015-12-15 22:55:29 +1100

    Added battery display

--- src/main.c
+++ src/main.c
@@ -1,57 +1,26 @@
 #include <pebble.h>
 
 static Window *s_main_window;
-static TextLayer *s_time_layer;
+static TextLayer *s_time_layer; // 12 hour time layer
 static TextLayer *t_time_layer; //24 hour time layer
-static TextLayer *background_first_half; //colour of the background
+static TextLayer *text; //The text displayed in the bottom right corner
+static TextLayer *battery_layer; //Battery display 
+
+// The layers for the background
+static TextLayer *background_first_half; 
 static TextLayer *background_second_half;
 
-static void main_window_load(Window *window) {
-  // Get information about the Window
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
-  
-  // Create the TextLayer with specific bounds
-  int vert_offset = bounds.size.h/2;
-  background_first_half = text_layer_create(GRect(0, 0, bounds.size.w, vert_offset));
-  background_second_half = text_layer_create(GRect(0, vert_offset, bounds.size.w, vert_offset));
-  
-  s_time_layer = text_layer_create(GRect(0, bounds.size.h/10, bounds.size.w, 50));
-  t_time_layer = text_layer_create(GRect(0, bounds.size.h/10 + vert_offset, bounds.size.w, 50));
-  
-  
-  // Improve the layout to be more like a watchface
-  text_layer_set_background_color(background_first_half, GColorOrange);
-  text_layer_set_background_color(background_second_half, GColorBlack);
 
+static void display_batt_attr(BatteryChargeState batt_state){
+  static char batt_status[40];
+  char percent = '%';
+  if(batt_state.is_charging){
+    snprintf(batt_status, sizeof(batt_status), "charging..."); 
+  }else{
+    snprintf(batt_status, sizeof(batt_status), "%d%c", batt_state.charge_percent, percent);
+  }
   
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  
-  text_layer_set_background_color(t_time_layer, GColorClear);
-  text_layer_set_text_color(t_time_layer, GColorOrange);
-  text_layer_set_text(t_time_layer, "00:00");
-  text_layer_set_font(t_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(t_time_layer, GTextAlignmentCenter);
-  
-  
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(background_first_half));
-  layer_add_child(window_layer, text_layer_get_layer(background_second_half));
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
-  layer_add_child(window_layer, text_layer_get_layer(t_time_layer));
-
-  
-}
-
-static void main_window_unload(Window *window) {
-  text_layer_destroy(s_time_layer);
-  text_layer_destroy(t_time_layer);
-  text_layer_destroy(background_first_half);
-  text_layer_destroy(background_second_half);
+  text_layer_set_text(battery_layer, batt_status); 
 
 }
 
@@ -67,6 +36,9 @@ static void update_time(){
   strftime(s_buffer, sizeof(s_buffer), "%I:%M", tick_time);
   strftime(t_buffer, sizeof(s_buffer), "%H:%M", tick_time);
 
+  int min = tick_time->tm_sec;
+  if(min % 2 ==0){text_layer_set_text(text, "Pebble");}
+  else if (min %2 == 1){text_layer_set_text(text, "Time");}
   
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
@@ -77,12 +49,97 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
     update_time();
 }
 
+
+static void main_window_load(Window *window) {
+  // Get information about the Window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  
+  int vert_offset = bounds.size.h/2;
+  
+  // Background
+  background_first_half = text_layer_create(GRect(0, 0, bounds.size.w, vert_offset));
+  background_second_half = text_layer_create(GRect(0, vert_offset, bounds.size.w, vert_offset));
+  
+  // Battery
+  battery_layer = text_layer_create(GRect(40, 0, 80, 50));
+  
+  // Time Displays
+  s_time_layer = text_layer_create(GRect(0, bounds.size.h/10, bounds.size.w, 50));
+  t_time_layer = text_layer_create(GRect(0, bounds.size.h/10 + vert_offset, bounds.size.w, 50));
+  
+  // Text Display
+  text = text_layer_create(GRect(bounds.size.w * 0.4 + 5, bounds.size.h * 0.9, 0.8 * bounds.size.w, 50));
+  
+  
+  // Background
+  text_layer_set_background_color(background_first_half, GColorOrange);
+  text_layer_set_background_color(background_second_half, GColorBlack);
+  
+  // Battery
+  text_layer_set_background_color(battery_layer, GColorClear);
+  text_layer_set_text_color(battery_layer, GColorBlack);
+  text_layer_set_font(battery_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  text_layer_set_text_alignment(battery_layer, GTextAlignmentCenter);
+  text_layer_set_text(battery_layer, "000%");
+  
+  // 12 - hour time
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorBlack);
+  text_layer_set_text(s_time_layer, "00:00");
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  
+  // 24 - hour time
+  text_layer_set_background_color(t_time_layer, GColorClear);
+  text_layer_set_text_color(t_time_layer, GColorOrange);
+  text_layer_set_text(t_time_layer, "00:00");
+  text_layer_set_font(t_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT));
+  text_layer_set_text_alignment(t_time_layer, GTextAlignmentCenter);
+  
+  // Text display
+  text_layer_set_background_color(text, GColorClear);
+  text_layer_set_text_color(text, GColorLightGray);
+  text_layer_set_font(text, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  text_layer_set_text_alignment(text, GTextAlignmentCenter);
+  
+  
+  
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(background_first_half));
+  layer_add_child(window_layer, text_layer_get_layer(background_second_half));
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(t_time_layer));
+  layer_add_child(window_layer, text_layer_get_layer(text));
+  layer_add_child(window_layer, text_layer_get_layer(battery_layer));
+  
+  // Handlers
+  display_batt_attr(battery_state_service_peek());
+
+
+  
+}
+
+static void main_window_unload(Window *window) {
+  text_layer_destroy(s_time_layer);
+  text_layer_destroy(t_time_layer);
+  text_layer_destroy(text);
+  text_layer_destroy(battery_layer);
+  text_layer_destroy(background_first_half);
+  text_layer_destroy(background_second_half);
+
+}
+
+
 static void init() {
   // Create main Window element and assign to pointer
   s_main_window = window_create();
   
   //Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  tick_timer_service_subscribe(SECOND_UNIT, tick_handler);
+  
+  //Register with BatteryStateService
+  battery_state_service_subscribe(display_batt_attr);
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -93,7 +150,7 @@ static void init() {
   // Show the Window on the watch, with animated=true
   window_stack_push(s_main_window, true);
   
-  // Make sure the time is displayed from the start
+  // Init event handlers
   update_time();
 }
 
